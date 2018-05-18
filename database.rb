bf37d09664c084681de5243173f4bab1862674d7
Khang Nguyen <khang.nguyen@arag.be>
2018-05-18 10:19:35 +0200

    Create table if not exist on database initialization

--- database.rb
+++ database.rb
@@ -4,6 +4,15 @@ class Database
   def initialize()
     @db = SQLite3::Database.new("checking-system.db")
     @db.results_as_hash = true
+    @db.execute "CREATE TABLE IF NOT EXISTS credentials(
+        card_id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL, 
+        password TEXT NOT NULL,
+        state INTEGER NOT NULL DEFAULT 0,
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+        updated_at TEXT DEFAULT CURRENT_TIMESTAMP
+      )
+    "
   end
 
   def insert(cardId, userName, password)
